9bee39e97358b6db2d93cf2d741e8410e43d07cf
Abdisamad6378 <abdisamadhussein77@gmail.com>
2026-01-20 14:38:57 +0300

    changes in  task2.sql file

--- task_2.sql
+++ task_2.sql
@@ -18,3 +18,10 @@ CREATE TABLE Customers (
     email VARCHAR(215),
     address TEXT
 );
+
+CREATE TABLE Orders (
+    order_id INT PRIMARY KEY,
+    order_date DATE,
+    customer_id INT,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
