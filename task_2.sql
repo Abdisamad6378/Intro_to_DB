5ffe6c77c8951c2eb043409e4ba5eaeeca352937
Abdisamad6378 <abdisamadhussein77@gmail.com>
2026-01-20 14:35:17 +0300

    changes in  task2.sql file

--- task_2.sql
+++ task_2.sql
@@ -2,3 +2,12 @@ CREATE TABLE Authors (
     author_id INT,
     author_name VARCHAR(100)
 );
+
+-- Create Books table
+CREATE TABLE Books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(255),
+    price DECIMAL(10, 2),
+    author_id INT,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
